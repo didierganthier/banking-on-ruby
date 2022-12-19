1f383a35b776ed0195312a28c7022d7d2553e501
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-12-19 20:43:59 +0000

    Fix linters

--- account.rb
+++ account.rb
@@ -1,34 +1,35 @@
 class Account
-    attr_reader :name
-    attr_reader :balance
-  
-    def initialize(name, balance=100)
-      @name = name
-      @balance = balance
-    end
+  attr_reader :name, :balance
 
-    def display_balance(pin_number)
-        puts pin_number == pin ? "Balance: $#{@balance}." : pin_error
-    end
+  def initialize(name, balance = 100)
+    @name = name
+    @balance = balance
+  end
 
-    def withdraw(pin_number, amount)
-        if pin_number == pin
-          @balance -= amount
-          puts "Withdrew #{amount}. New balance: $#{@balance}."
-        else
-          puts pin_error
-        end
-    end
+  def display_balance(pin_number)
+    puts pin_number == pin ? "Balance: $#{@balance}." : pin_error
+  end
 
-    private
-    def pin
-      @pin = 1234
+  def withdraw(pin_number, amount)
+    if pin_number == pin
+      @balance -= amount
+      puts "Withdrew #{amount}. New balance: $#{@balance}."
+    else
+      puts pin_error
     end
-  
-    private
-    def pin_error
-      "Access denied: incorrect PIN."
-    end
+  end
+
+  private
+
+  def pin
+    @pin = 1234
+  end
+
+  private
+
+  def pin_error
+    'Access denied: incorrect PIN.'
+  end
 end
 
-checking_account = Account.new("Didier Ganthier Personal Account", "123456789")
+checking_account = Account.new('Didier Ganthier Personal Account', '123456789')
