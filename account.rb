def3d9a32cc9413abb3cd3eec4cd963c9dc5200b
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-12-19 20:22:16 +0000

    Withdraw method

--- account.rb
+++ account.rb
@@ -11,6 +11,15 @@ class Account
         puts pin_number == pin ? "Balance: $#{@balance}." : pin_error
     end
 
+    def withdraw(pin_number, amount)
+        if pin_number == pin
+          @balance -= amount
+          puts "Withdrew #{amount}. New balance: $#{@balance}."
+        else
+          puts pin_error
+        end
+    end
+
     private
     def pin
       @pin = 1234
